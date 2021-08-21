b741444c7c1a72140e0791b015096b6fe06c9022
AiperiAkim <aiperi.akim.gg@gmail.com>
2021-08-21 17:45:36 +0600

    article

--- src/components/pages/Furnitura/Article.js
+++ src/components/pages/Furnitura/Article.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Article.css';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles({
@@ -11,73 +11,74 @@ const useStyles = makeStyles({
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     color: 'white',
-    width:'180px',
+    width: '180px',
     height: 48,
     margin: 10,
    
   },
 });
 export default function Article() {
-    
-    const classes = useStyles();
+  const classes = useStyles();
    
-        return (
+  return (
             
     
-        <>
-        <div className="container">
+    <>
+      <div className="container">
         <div className="prihod">
           <h1> Артикул</h1>
           ********************************
          
-  <Button href="/ArticlePage" className={classes.root}>Добавить</Button> 
-  <Button className={classes.root}>Обновить</Button> 
-  <Button  href="/DeletePage" className={classes.root}>Удалить</Button>
-  <Button  href="/Furnitura" className={classes.root}>Назад</Button>
-   </div>
-    <div className="tab">
-            <table  className="tab-style">
-<thead className="shapka">
-<tr>
-<th>Наименование</th>
-<th>Размерный ряд</th>
-<th>Тех харак</th>
-<th>Дата начала</th>
-<th>Количество</th>
-<th>Дата окончания</th>
-<th>Цена</th>
-<th>Контрагенты</th>
-</tr>
-</thead>
-<tbody>
+          <Button href="/ArticlePage" className={classes.root}>
+            Добавить
+          </Button> 
+          <Button className={classes.root}>Обновить</Button> 
+          <Button href="/DeletePage" className={classes.root}>Удалить</Button>
+          <Button href="/Furnitura" className={classes.root}>Назад</Button>
+        </div>
+        <div className="tab">
+          <table className="tab-style">
+            <thead className="shapka">
+              <tr>
+                <th>Наименование</th>
+                <th>Размерный ряд</th>
+                <th>Тех харак</th>
+                <th>Дата начала</th>
+                <th>Количество</th>
+                <th>Дата окончания</th>
+                <th>Цена</th>
+                <th>Контрагенты</th>
+              </tr>
+            </thead>
+            <tbody>
 
-<tr>
-<td>CВ-516</td>
-<td>5</td>
-<td>Детские шорты</td>
-<td>2021-01-01</td>
-<td>30000</td>
-<td>0</td>
-<td>65</td>
-<td>Trade center</td>
-</tr>
+              <tr>
+                <td>CВ-516</td>
+                <td>5</td>
+                <td>Детские шорты</td>
+                <td>2021-01-01</td>
+                <td>30000</td>
+                <td>0</td>
+                <td>65</td>
+                <td>Trade center</td>
+              </tr>
 
-<tr>
-<td>Т-21</td>
-<td>5</td>
-<td>Безрукавка</td>
-<td>2021-01-01</td>
-<td>80000</td>
-<td>0</td>
-<td>65</td>
-<td>Ту Гуен</td>
-</tr>
+              <tr>
+                <td>Т-21</td>
+                <td>5</td>
+                <td>Безрукавка</td>
+                <td>2021-01-01</td>
+                <td>80000</td>
+                <td>0</td>
+                <td>65</td>
+                <td>Ту Гуен</td>
+              </tr>
 
-</tbody>
-</table>
-</div>
-</div>
-</>
-        )
-        }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
 
--- src/components/pages/Furnitura/PrihodPage.js
+++ src/components/pages/Furnitura/PrihodPage.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import './PrihodPage.css';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
-import { FormControlLabel } from '@material-ui/core';
+import {FormControlLabel} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -20,110 +20,114 @@ const useStyles = makeStyles((theme) =>
 );
 
 
+export default function PrihodPage() {
+  const classes = useStyles();  
+  const [value, setValue] = useState('prihod');
 
-export default function PrihodPage()  {
- 
-    const classes = useStyles();  
-    const [value, setValue] = useState('prihod');
+  // const [name, setName] = useState('')
 
-    // const [name, setName] = useState('')
+  // const nameChangeHandler = (e) => {
+  //   setName(e.target.value)
+  // }
 
-    // const nameChangeHandler = (e) => {
-    //   setName(e.target.value)
-    // }
+  // const onSubmit = async () => {
 
-    // const onSubmit = async () => {
+  //   const form = {
+  //     name: name
+  //   }
 
-    //   const form = {
-    //     name: name
-    //   }
+  //   const response = await fetch('https://www.fsf.com/', {
+  //     headers: {
+  //       'Authorization': 'Token fdsfasdf',
+  //     },
+  //     body: JSON.stringify(form)
+  //   })
 
-    //   const response = await fetch('https://www.fsf.com/', {
-    //     headers: {
-    //       'Authorization': 'Token fdsfasdf',
-    //     },
-    //     body: JSON.stringify(form)
-    //   })
+  //   const data = await response.json()
 
-    //   const data = await response.json()
-
-    //   console.log('data: ', data)
-    // }
+  //   console.log('data: ', data)
+  // }
 
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
-    return (
-      <>
+  return (
+    <>
       <div className="form-prihod">
         <h1>Заполните данные</h1>
         
-      <form className={classes.root} noValidate autoComplete="off">
-      <TextField
-        id="outlined-secondary"
-        label="Наименование(тех.харак)"
-        variant="outlined"
-        color="secondary"
-        // value={name}
-        // onChange={nameChangeHandler}
-      />
-        <TextField
-        id="outlined-secondary"
-        label="Код"
-        variant="outlined"
-        color="secondary"
-      />
-     <TextField
-        id="outlined-secondary"
-        label="Количество"
-        variant="outlined"
-        color="secondary"
-      />
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-secondary"
+            label="Наименование(тех.харак)"
+            variant="outlined"
+            color="secondary"
+            // value={name}
+            // onChange={nameChangeHandler}
+          />
+          <TextField
+            id="outlined-secondary"
+            label="Код"
+            variant="outlined"
+            color="secondary"
+          />
+          <TextField
+            id="outlined-secondary"
+            label="Количество"
+            variant="outlined"
+            color="secondary"
+          />
       
-      <RadioGroup aria-label="prihod-rashod" name="pr" value={value} onChange={handleChange}>
-        <FormControlLabel value="prihod" control={<Radio />} label="Приход" />
-        <FormControlLabel value="rashod" control={<Radio />} label="Расход" />
+          <RadioGroup aria-label="prihod-rashod" 
+            name="pr" value={value} onChange={handleChange}>
+            <FormControlLabel value="prihod" 
+              control={<Radio />} label="Приход" />
+            <FormControlLabel value="rashod" 
+              control={<Radio />} label="Расход" />
         
-      </RadioGroup>
+          </RadioGroup>
     
-        <TextField
-        id="outlined-secondary"
-        label="Адрес"
-        variant="outlined"
-        color="secondary"
-      />
-        <TextField
-        id="outlined-secondary"
-        label="Дата"
-        variant="outlined"
-        color="secondary"
-      />
+          <TextField
+            id="outlined-secondary"
+            label="Адрес"
+            variant="outlined"
+            color="secondary"
+          />
+          <TextField
+            id="outlined-secondary"
+            label="Дата"
+            variant="outlined"
+            color="secondary"
+          />
      
-      <TextField
-        id="outlined-secondary"
-        label="ед.изм"
-        variant="outlined"
-        color="secondary"
-      />
-      <TextField
-        id="outlined-secondary"
-        label="Цена"
-        variant="outlined"
-        color="secondary"
-      />
-      <TextField
-        id="outlined-secondary"
-        label="Артикул"
-        variant="outlined"
-        color="secondary"
-      />
-      <Button variant="contained" color="secondary"  size="large" href="/Prihod" >
+          <TextField
+            id="outlined-secondary"
+            label="ед.изм"
+            variant="outlined"
+            color="secondary"
+          />
+          <TextField
+            id="outlined-secondary"
+            label="Цена"
+            variant="outlined"
+            color="secondary"
+          />
+          <TextField
+            id="outlined-secondary"
+            label="Артикул"
+            variant="outlined"
+            color="secondary"
+          />
+          <Button variant="contained" 
+            color="secondary" 
+            size="large" 
+            href="/Prihod" >
   Сохранить
-</Button>
-      </form>
+          </Button>
+        </form>
       </div>
-      </>
-    );
-  }
+    </>
+  );
+}
 
