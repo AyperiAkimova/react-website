3442f8380e76e7318fc3c2552fe9cae22bf4dedc
sravshanbek <sravshanbek@it.gov.kg>
2021-08-21 16:18:59 +0600

    arhitecture

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,15 +7,15 @@ const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
 const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({
-    children, 
-    type, 
-    onClick, 
-    buttonStyle, 
-    buttonSize
+  children,
+  type, 
+  onClick, 
+  buttonStyle,
+  buttonSize,
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
+  const checkButtonStyle = STYLES.includes(buttonStyle) ?
+    buttonStyle :
+    STYLES[0];
 
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
@@ -31,5 +31,5 @@ export const Button = ({
      
     </Link>
     
-    )
+  );
 };
